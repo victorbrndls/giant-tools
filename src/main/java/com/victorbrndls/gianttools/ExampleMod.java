d927b8b3b3be36b672edaac1eeccc83a37a65f13
Victor B <haristolhogamer@gmail.com>
2022-04-12 17:50:02 -0300

    Rename modid to gianttools

--- src/main/java/com/victorbrndls/gianttools/ExampleMod.java
+++ src/main/java/com/victorbrndls/gianttools/ExampleMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.victorbrndls.gianttools;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.world.level.block.Block;
@@ -18,7 +18,7 @@ import org.slf4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
+@Mod("gianttools")
 public class ExampleMod
 {
     // Directly reference a slf4j logger
